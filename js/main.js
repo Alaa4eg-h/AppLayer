332d0e31a038ba0157cd3f0978ce10fbefccaa96
Alaa4eg-h <Alaa4eg@outlook.com>
2021-04-01 01:08:04 +0200

    Add Js Code To The Template

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ const header = document.querySelector('.header');
 const logoImg = document.querySelector('.logo-img')
 const navLink = document.querySelectorAll('.nav-link');
 const navList = document.querySelector('.nav-list');
+const arrows = document.querySelectorAll('.accordion .icon')
 
 
 // ADD/REMOVE CLASS "OPEN-MENU" WHEN CLICKING TOGGELER MENU
@@ -33,7 +34,6 @@ window.addEventListener('scroll', () => {
 
 
 // ADD SMOOTH SCROLLING WHEN SELECT THE NAV LINK
-
 // FIRST ADD CLICK EVENT TO ALL LINKS
 navLink.forEach((link) => {
     link.addEventListener('click', (e) => {
@@ -44,14 +44,22 @@ navLink.forEach((link) => {
             top: offsetTop,
             behavior: "smooth"
         })
-
         // REMOVE "OPEN-MENU" WHEN TAGRET A LINK
         header.classList.remove('menu-open');
-        // for (link of navLink) {
-        //     link.remove('active');
-        // }
         navList.querySelector('.active').classList.remove('active');
         link.classList.add('active');
-
     })
 })
+
+arrows.forEach((r) => {
+    r.addEventListener('click', (e) => {
+        e.preventDefault();
+        const arrowParent = r.parentElement.parentElement;
+        if (arrowParent.classList.contains("active")) {
+            arrowParent.classList.toggle('active');
+        }
+    })
+})
+
+
+
